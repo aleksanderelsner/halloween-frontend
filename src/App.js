8566ceeae0517ce76f0d4cd298226545abbf5cc9
Aleksander Elsner <aelsner@box.com>
2025-09-09 20:22:40 +0200

    form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,75 @@
 import './styles/App.css';
-import { Button } from "react-bootstrap";
+import {Button, Form} from "react-bootstrap";
+import {useState} from "react";
 
 function App() {
 	const backendUrl = process.env.REACT_APP_BACKEND_URL;
-	const callApi = () => fetch(backendUrl + '/costumes')
-		.then(response => response.json())
-		.then(data => console.log(data))
-		.catch(error => console.error('Error fetching data:', error));
+	const [inputValue, setInputValue] = useState('');
+	const [email, setEmail] = useState('');
+	const [costumes, setCostumes] = useState([]);
+
+	const handleChangeState = (event) => {
+		const value = event.target.value;
+		if (costumes.length > 4) {
+			alert("kurwa nie przesadzaj z tymi kostiumami, 5 ci starczy łapciuchu");
+			return;
+		}
+		setInputValue(value);
+	}
+
+	const handleKeyDown = (event) => {
+		if (event.key === 'Enter' && inputValue.trim() !== '') {
+			event.preventDefault();
+			setCostumes([...costumes, inputValue.trim()]);
+			setInputValue('');
+		}
+	}
+
+	const resetCostumes = () => {
+		setCostumes([]);
+	}
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		console.log("calling api");
+		const resp = await fetch(backendUrl + '/costumes', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ email, costumes }),
+		})
+		console.log(resp);
+		setInputValue('');
+		setCostumes([]);
+		setEmail('');
+	}
+
   return (
     <div className="App">
-		<Button variant="primary" onClick={callApi}>TEST</Button>
+		<Form onSubmit={handleSubmit} className="Form">
+			<Form.Group className="mb-3" controlId="formBasicEmail">
+				<Form.Label column="lg">Email</Form.Label>
+				<Form.Control
+						type="email"
+						placeholder="Zapodaj email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+				/>
+				<Form.Label column="lg">Dodaj kostium</Form.Label>
+				<Form.Control
+						type="text"
+						placeholder="Kostiumy"
+						onChange={handleChangeState}
+						onKeyDown={handleKeyDown}
+						value={inputValue}
+				/>
+				<Form.Label column="sm">{ costumes.join(', ') }</Form.Label>
+			</Form.Group>
+			<Button variant="primary"
+					type="submit">Wyślij</Button>
+			<Button variant="secondary" onClick={resetCostumes}>Resetuj</Button>
+		</Form>
     </div>
   );
 }
