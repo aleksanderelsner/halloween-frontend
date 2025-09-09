6f6b3c3f955baab915a989fbe34bfd0f2fb0903e
Aleksander Elsner <aelsner@box.com>
2025-09-09 15:24:54 +0200

    use sass and call api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import './styles/App.css';
+import { Button } from "react-bootstrap";
 
 function App() {
+	const backendUrl = process.env.REACT_APP_BACKEND_URL;
+	const callApi = () => fetch(backendUrl + '/costumes')
+		.then(response => response.json())
+		.then(data => console.log(data))
+		.catch(error => console.error('Error fetching data:', error));
   return (
     <div className="App">
-		Test
+		<Button variant="primary" onClick={callApi}>TEST</Button>
     </div>
   );
 }
